a6bad2020e984c6ec777d0c16cec2c090dd742d0
Humayl Chishti <humaylchishti@Humayls-MacBook-Pro.local>
2024-10-04 18:36:50 -0700

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+    question: "Who was the first president of The United States?",
+    option1: 'George Washington',
+    option2: 'Abraham Lincoln',
+    option3: 'George Bush',
+    option4: 'Donald Trump',
+    ans: 1,
+},
+{
+    question: "How many countries are there in the world?",
+    option1: '150',
+    option2: '180',
+    option3: '187',
+    option4: '195',
+    ans: 4,
+},
+{
+    question: "Who has the most NBA championships?",
+    option1: 'Lakers',
+    option2: 'Blazers',
+    option3: 'Bulls',
+    option4: 'Celtics',
+    ans: 4,
+},
+{
+    question: "Who is the richest man in the world?",
+    option1: 'Jeff Bezos',
+    option2: 'Mark Zuckerberg',
+    option3: 'Elon Musk',
+    option4: 'Donald Trump',
+    ans: 3,
+},
+{
+    question: "What is the biggest continent",
+    option1: 'North America',
+    option2: 'Africa',
+    option3: 'Asia',
+    option4: 'Europe',
+    ans: 3,
+}
+]
